cbc8f77e09bba2f2973f4e6c8d0b7dd6b269f86a
Valeriano Della Longa <valeriano.dellalonga@me.com>
2021-02-01 19:07:18 +0100

    Added inline help

--- Sources/LLRBTree/LLRBTree.swift
+++ Sources/LLRBTree/LLRBTree.swift
@@ -33,6 +33,9 @@ import Foundation
 public final class LLRBTree<Key: Comparable, Value>: NSCopying {
     var root: Node? = nil
     
+    /// Instantiates and returns a new empty Left-Leaning Red-Black Tree.
+    ///
+    /// - Returns: A new empty Left-Leaning Red-Black Tree instance.
     public init() { }
     
     public func copy(with zone: NSZone? = nil) -> Any {
@@ -48,6 +51,17 @@ extension LLRBTree: ExpressibleByDictionaryLiteral {
         self.init(elements)
     }
     
+    /// Returns a new instance initialized to contain all elements from given sequence.
+    ///
+    /// - Parameter elements:   A sequence containing key/value paris as elements to
+    ///                         store in the new Left-Leaning Red-Black Tree instance.
+    /// - Returns:  A new Left-Leaning Red-Black Tree instance containing all elements in
+    ///             the given `elements` sequence.
+    /// - Complexity:   O(*n*log*n*) where *n* is the number of elements contained
+    ///                 in the given sequience.
+    /// - Note: When the given sequence contains duplicate keys values in its elements,
+    ///         the last element's value with the duplicate key willbe stored in the returned
+    ///         instance.
     public convenience init<S: Sequence>(_ elements: S) where S.Element == (Key, Value) {
         if let other = elements as? LLRBTree<Key, Value> {
             self.init(other)
@@ -76,29 +90,76 @@ extension LLRBTree: ExpressibleByDictionaryLiteral {
 
 // MARK: - Computed properties
 extension LLRBTree {
+    /// The number of elements stored in this Left-Leaning Red-Black Tree instance.
+    /// - Complexity: O(1)
     public var count: Int { root?.count ?? 0 }
     
+    /// A boolean value, `true` when no element is stored in this
+    /// Left-Leaning Red-Black Tree instance.
+    /// - Complexity: O(1)
     public var isEmpty: Bool { root == nil }
     
+    /// The element with the smallest key stored in this Left-Leaning Red-Black Tree instance.
+    /// Returns `nil` when `isEmpty` is `true`.
+    /// - Complexity:   O(log *n*) where *n* is the number of stored elements in
+    ///                 this Left-Leaning Red-Black Tree instance.
     public var min: Element? {
         guard let rootMin = root?.min else { return nil }
         
         return (rootMin.key, rootMin.value)
     }
     
+    /// The element with the greatest key stored in this Left-Leaning Red-Black Tree instance.
+    /// Returns `nil` when `isEmpty` is `true`.
+    /// - Complexity:   O(log *n*) where *n* is the number of stored elements in
+    ///                 this Left-Leaning Red-Black Tree instance.
     public var max: Element? {
         guard let rootMax = root?.max else { return nil }
         
         return (rootMax.key, rootMax.value)
     }
-    
+    /// The smallest key stored in this Left-Leaning Red-Black Tree instance.
+    /// Returns `nil` when `isEmpty` is `true`.
+    /// - Complexity:   O(log *n*) where *n* is the number of stored elements in
+    ///                 this Left-Leaning Red-Black Tree instance.
     public var minKey: Key? { root?.minKey }
     
+    /// The greatest key stored in this Left-Leaning Red-Black Tree instance.
+    /// Returns `nil` when `isEmpty` is `true`.
+    /// - Complexity:   O(log *n*) where *n* is the number of stored elements in
+    ///                 this Left-Leaning Red-Black Tree instance.
     public var maxKey: Key? { root?.maxKey }
 }
 
 // MARK: - subscript and key/value based operations
 extension LLRBTree {
+    /// Access elements' values stored in this Left-Leaning Red-Black Tree instance, via keys.
+    ///
+    /// - Parameter key: The key of the element to access.
+    /// - Returns: The value stored for the given key or `nil` if such key is not present.
+    /// - Note: The subscript can be used to update, insert or delete an element:
+    ///         ```
+    ///         var tree = LLRBTree<String, Int>()
+    ///         tree["A"] = 1
+    ///         // ("A", 1) is inserted in tree
+    ///
+    ///         print("\(tree["A"] ?? "nil")")
+    ///         // prints 1
+    ///
+    ///         tree["A"] = 3
+    ///         // element with key "A" get its value updated to 3
+    ///
+    ///         print("\(tree["A"] ?? "nil")")
+    ///         // prints 3
+    ///
+    ///         tree["A"] = nil
+    ///         // removes the element with key "A"
+    ///
+    ///         print("\(tree["A"] ?? "nil")")
+    ///         // prints nil
+    ///         ```
+    /// - Complexity:   Amortized O(log *n*) where *n* is the count of
+    ///                 this Left-Leaning Red-Black Tree instance.
     public subscript(key: Key) -> Value? {
         get {
             value(forKey: key)
@@ -113,10 +174,30 @@ extension LLRBTree {
         }
     }
     
+    /// Get the value stored for given key, if such key exists in
+    /// this Left-Leaning Red-Black Tree instance.
+    ///
+    /// - Parameter forKey: The key to use for retrieving the element's value.
+    /// - Returns:  The value stored in the element with such given key, `nil` if such
+    ///             element does not exist in this Left-Leaning Red-Black Tree instance.
+    /// - Complexity:   O(log *n*) where *n* is the count of
+    ///                 this Left-Leaning Red-Black Tree instance.
     public func value(forKey key: Key) -> Value? {
         root?.value(forKey: key)
     }
     
+    /// Set the given value for the given key. If such key exists in
+    /// this Left-Leaning Red-Black Tree instance then the element's value with such key
+    /// gets updated to the given value; otherwise a new element with the given
+    /// key/value pair gets created and inserted in the tree.
+    ///
+    /// - Parameter value: the new value to set for the given key.
+    /// - Parameter forKey: The key to use for retrieving the element's value or
+    ///                     to insert in the tree if it doesn't exixts.
+    /// - Returns:  The value stored in the element with such given key, `nil` if such
+    ///             element does not exist in this Left-Leaning Red-Black Tree instance.
+    /// - Complexity:   Amortized O(log *n*) where *n* is the count of
+    ///                 this Left-Leaning Red-Black Tree instance.
     public func setValue(_ value: Value, forKey key: Key) {
         if let r = root {
             r.setValue(value, forKey: key)
@@ -126,16 +207,31 @@ extension LLRBTree {
         root!.color = .black
     }
     
+    /// Removes the element with the given key from this Left-Leaning Red-Black Tree instance.
+    ///
+    /// - Parameter forKey: The key of the element to remove.
+    /// - Complexity:   Amortized O(log *n*) where *n* is the count of
+    ///                 this Left-Leaning Red-Black Tree instance.
     public func removeValue(forKey key: Key) {
         root = root?.removingValue(forKey: key)
         root?.color = .black
     }
     
+    /// Removes the element with the smallest key from
+    /// this Left-Leaning Red-Black Tree instance.
+    ///
+    /// - Complexity:   Amortized O(log *n*) where *n* is the count of
+    ///                 this Left-Leaning Red-Black Tree instance.
     public func removeValueForMinKey() {
         root = root?.removingValueForMinKey()
         root?.color = .black
     }
     
+    /// Removes the element with the greatest key from
+    /// this Left-Leaning Red-Black Tree instance.
+    ///
+    /// - Complexity:   Amortized O(log *n*) where *n* is the count of
+    ///                 this Left-Leaning Red-Black Tree instance.
     public func removeValueForMaxKey() {
         root = root?.removingValueForMaxKey()
         root?.color = .black
@@ -207,6 +303,18 @@ extension LLRBTree: Sequence {
 
 // MARK: - Additional FP methods
 extension LLRBTree {
+    /// Returns a new Left-Leaning Red-Black Tree containing the keys of this
+    /// Left-Leaning Red-Black Tree with the values transformed by the given closure.
+    ///
+    /// - Parameter transform:  A closure that transforms a value.
+    ///                         transform accepts each value of the
+    ///                         Left-Leaning Red-Black Tree as its
+    ///                         parameter and returns a transformed value
+    ///                         of the same or of a different type.
+    /// - Returns:  A Left-Leaning Red-Black Tree containing the keys and
+    ///             transformed values of this Left-Leaning Red-Black Tree.
+    /// - Complexity:   O(*n*) where *n* is the count of elements
+    ///                 in this Left-Leaning Red-Black Tree.
     public func mapValues<T>(_ transform: (Value) throws -> T) rethrows -> LLRBTree<Key, T> {
         let mappedRoot = try root?.mapValues(transform)
         let transformed = LLRBTree<Key, T>()
@@ -215,6 +323,43 @@ extension LLRBTree {
         return transformed
     }
     
+    /// Returns a new Left-Leaning Red-Black Tree containing only the key-value
+    /// pairs that have non-nil values as the result of transformation by the given
+    /// closure.
+    ///
+    /// Use this method to receive a Left-Leaning Red-Black Tree  with non-optional
+    /// values when your transformation produces optional values.
+    ///
+    /// In this example, note the difference in the result of using mapValues and
+    /// compactMapValues with a transformation that returns an optional Int value.
+    /// ```
+    ///     let data: LLRBTree = [
+    ///         "a": "1",
+    ///         "b": "three",
+    ///         "c": "///4///"
+    ///     ]
+    ///
+    ///     let m: LLRBTree<String, Int?> = data
+    ///         .mapValues { str in Int(str) }
+    ///     // ["a": 1, "b": nil, "c": nil]
+    ///
+    ///     let c: LLRBTRee<String,Int> = data
+    ///         .compactMapValues { str in Int(str) }
+    ///     // ["a": 1]
+    /// ```
+    /// - Parameter transform:  A closure that transforms a value.
+    ///                         transform accepts each value of the
+    ///                         Left-Leaning Red-Black Tree as its
+    ///                         parameter and returns an optional
+    ///                         transformed value of the same or of a
+    ///                         different type.
+    /// - Returns:  A Left-Leaning Red-Black Tree containing the keys and
+    ///             non-nil transformed values of this
+    ///             Left-Leaning Red-Black Tree.
+    /// - Complexity:   O(*m* + *n*) where *n* is the count of elements
+    ///                 in this Left-Leaning Red-Black Tree,
+    ///                 and *m* is the count of the resulting
+    ///                 Left_Leaning Red-Black Tree.
     public func compactMapValues<T>(_ transform: (Value) throws -> T?) rethrows -> LLRBTree<Key, T> {
         let transformed = LLRBTree<Key, T>()
         try root?.forEach { element in
